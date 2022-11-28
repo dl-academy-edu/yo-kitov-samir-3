1dd781f512b120b69bf991b16f656560df3904ab
Samir <sakitov@yandex.ru>
2022-11-28 23:25:22 +0300

    Сделал два варианта с условием и декаратором, не знаю какой лучше

--- source/js/sliderbanner.js
+++ source/js/sliderbanner.js
@@ -1,23 +1,73 @@
-import {createControls} from './utils.js';
+import {createControls, hideSlide, showSlide, setHeightParent} from './utils.js';
 
 if (document.querySelector('.slider__list')) {
   const parentSlides = document.querySelector('.slider__list');
-  const parentControls = document.querySelector('.controls__list');
+  const listControls = document.querySelector('.controls__list');
+  const doxControls = document.querySelector('.controls');
 
-  const SLIDE_ACTIVE = '.slider__item--active';
-  const CONTROL_ACTIVE = 'controls__item--active';
-  const CONTROL = '.controls__item';
+  const SLIDE_ACTIVE = 'slider__item--active';
+  const SLIDE_DISABLED = 'slider__item--disabled';
+  const BUTTON_ACTIVE = 'controls__button--active';
+  const BUTTON = '.controls__button';
+  const CONTROL_ITEM = '.controls__item';
 
   const optionsSlider = {
-    start: 1 + [].indexOf.call(parentSlides.children, parentSlides.querySelector(SLIDE_ACTIVE)),
+    start: [].indexOf.call(parentSlides.children, parentSlides.querySelector(`.${SLIDE_ACTIVE}`)),
     step: 1,
-    counter: 0
+    counter: 0,
+    prevCounter: [].indexOf.call(parentSlides.children, parentSlides.querySelector(`.${SLIDE_ACTIVE}`))
   };
 
-  const controls = createControls(parentSlides.children.length, CONTROL, CONTROL_ACTIVE, optionsSlider.start);
+  const optionsSlide = {
+    parentSlides: parentSlides,
+    selectorActive: SLIDE_ACTIVE,
+    selectorHidden: SLIDE_DISABLED
+  };
 
-  parentControls.replaceChildren();
-  parentControls.append(...controls);
+  [].forEach.call(parentSlides.children, (item) => {
+    if (!item.classList.contains(SLIDE_ACTIVE) && !item.classList.contains(SLIDE_DISABLED)) {
+      item.classList.add(SLIDE_DISABLED);
+    }
+  });
 
-  [].forEach.call()
+  // setHeightParent(parentSlides, optionsSlider.start);
+
+  const controls = createControls(parentSlides.children.length, CONTROL_ITEM, BUTTON_ACTIVE, optionsSlider.start);
+
+  listControls.replaceChildren();
+  listControls.append(...controls);
+
+  // let isCooldown = false;
+  doxControls.addEventListener('click', (e) => {
+    const target = e.target;
+
+    if (target.closest(BUTTON)) {
+      const data = Number(target.dataset.buttonNumber);
+
+      if (optionsSlider.prevCounter !== data) {
+        const numberOfHidden = parentSlides.querySelectorAll(`.${optionsSlide.selectorHidden}`);
+
+        if (numberOfHidden.length === parentSlides.children.length - 1) {
+          hideSlide(optionsSlide, optionsSlider.prevCounter, 500);
+          showSlide(optionsSlide, data, 500);
+
+          optionsSlider.prevCounter = data;
+          optionsSlider.counter = data;
+        }
+        // (function () {
+        //   if (isCooldown) return;
+        //   isCooldown = true;
+
+        // hideSlide(optionsSlide, optionsSlider.prevCounter, 500);
+        // showSlide(optionsSlide, data, 500);
+        //
+        // optionsSlider.prevCounter = data;
+        // optionsSlider.counter = data;
+
+        //   setTimeout(() => isCooldown = false, 510);
+        // })();
+      }
+    }
+
+  });
 }
--- source/js/utils.js
+++ source/js/utils.js
@@ -4,25 +4,57 @@ function createControls(numberControls, selectorItem, selectorActive, start) {
 
   if (!itemControl) return;
 
-  for (let i = 1; i <= numberControls; i++) {
+  for (let i = 0; i < numberControls; i++) {
     const item = itemControl.cloneNode(true);
     const buttonControl = item.querySelector('button');
 
-    buttonControl.setAttribute('aria-label', `Переключить на ${i} слайдер`);
-    item.dataset.buttonNumber = i.toString();
+    buttonControl.setAttribute('aria-label', `Переключить на ${i + 1} слайдер`);
+    buttonControl.dataset.buttonNumber = i.toString();
 
     // Делаем все кнопки слайдера не активными
-    if (item.classList.contains(selectorActive)) {
-      item.classList.remove(selectorActive);
+    if (buttonControl.classList.contains(selectorActive)) {
+      buttonControl.classList.remove(selectorActive);
     }
 
     result.push(item);
   }
 
   // делаем одну кнопку активной
-  result[start - 1].classList.add(selectorActive);
+  result[start].querySelector('button')
+               .classList
+               .add(selectorActive);
 
   return result;
 }
 
-export {createControls};
+function hideSlide({parentSlides, selectorActive, selectorHidden}, prevSlide, time) {
+  const previousSlide = parentSlides.children[prevSlide];
+
+  // Прячем текущий слайдер
+  previousSlide.classList.remove(selectorActive);
+  // Если слайд спрятан, то убераем его доступность
+  const timeout = setTimeout(() => {
+    if (!previousSlide.classList.contains(selectorActive)) {
+      previousSlide.classList.add(selectorHidden);
+    }
+    clearTimeout(timeout);
+  }, time);
+}
+
+function showSlide({parentSlides, selectorActive, selectorHidden}, numberSlide, time) {
+  const currentSlide = parentSlides.children[numberSlide];
+
+  // Делаем следующий слайд доступным
+  currentSlide.classList.remove(selectorHidden);
+  // Показываем следующий слайд
+  const timeout = setTimeout(() => {
+    currentSlide.classList.add(selectorActive);
+    clearTimeout(timeout);
+  }, time);
+}
+
+function setHeightParent(parentSlides, numberSlide) {
+  parentSlides.style.height = parentSlides.children[numberSlide].offsetHeight + 'px';
+}
+
+export {createControls, hideSlide, showSlide, setHeightParent};
