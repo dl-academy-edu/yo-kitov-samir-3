a74a05d25213b9d86906fd2bc3f7d4060530f37a
samir <sakitov@yandex.ru>
2023-01-16 11:25:51 +0400

    исправил ошибку

--- source/js/app.js
+++ source/js/app.js
@@ -1,7 +1,6 @@
 import './forms/form-profile/form-profile.js';
 import './blog/blog.js';
 import './slider-index/sliders.js';
-import './burgermenu.js';
 import './swiper.js';
 import './index.js';
 
